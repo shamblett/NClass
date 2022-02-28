0b842030a337031f1a08051446f367fd3cbde7d5
Steve Hamblett <shamblett@gtsdevlan.com>
2022-02-28 15:43:57 +0000

    Dart project first cut

---
+++ src/Dart/Declarations/DartConstructorDeclaration.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.RegularExpressions;
+using NClass.Core;
+using NClass.Translations;
+
+namespace NClass.Dart
+{
+    public class DartConstructorDeclaration : IDartConstructorDeclaration
+    {
+        // [<access>] [static] <name>([<args>])
+        const string ConstructorPattern =
+            @"^\s*" + DartLanguage.AccessPattern + @"(?<static>static\s+)?" +
+            @"(?<name>" + DartLanguage.NamePattern + ")" +
+            @"\((?(static)|(?<args>.*))\)" + DartLanguage.DeclarationEnding;
+
+        static readonly Regex constructorRegex =
+            new Regex(ConstructorPattern, RegexOptions.ExplicitCapture);
+
+        private readonly Match match;
+
+        public DartConstructorDeclaration(Match match)
+        {
+            if (!match.Success)
+            {
+                throw new BadSyntaxException(Strings.ErrorInvalidDeclaration);
+            }
+
+            this.match = match;
+        }
+
+        public static DartConstructorDeclaration Create(string declaration)
+        {
+            var match = constructorRegex.Match(declaration);
+            return new DartConstructorDeclaration(match);
+        }
+
+        public string Name
+        {
+            get { return match.Groups["name"].Value; }
+        }
+
+        public string Type
+        {
+            get { return string.Empty; }
+        }
+
+        public AccessModifier AccessModifier
+        {
+            get { return DartLanguage.Instance.TryParseAccessModifier(match.Groups["access"].Value); }
+        }
+
+        public IArgumentListDeclaration<IParameterDeclaration> ArgumentList
+        {
+            get { return DartArgumentListDeclaration.Create(match.Groups["args"].Value); }
+        }
+
+        public bool IsStatic
+        {
+            get { return match.Groups["static"].Success; }
+        }
+    }
+}
---
+++ src/Dart/Declarations/DartEventNameDeclaration.cs
@@ -0,0 +1,42 @@
+﻿using NClass.Core;
+using NClass.Translations;
+using System.Text.RegularExpressions;
+
+namespace NClass.Dart
+{
+    public class DartEventNameDeclaration
+    {
+        const string EventNamePattern =
+            @"^\s*(?<name>" + DartLanguage.GenericOperationNamePattern + @")\s*$";
+
+        static readonly Regex nameRegex = new Regex(EventNamePattern, RegexOptions.ExplicitCapture);
+
+        readonly Match match;
+
+        public DartEventNameDeclaration(Match match)
+        {
+            if (!match.Success)
+            {
+                throw new BadSyntaxException(Strings.ErrorInvalidDeclaration);
+            }
+
+            this.match = match;
+        }
+
+        public static DartEventNameDeclaration Create(string declaration)
+        {
+            var match = nameRegex.Match(declaration);
+            return new DartEventNameDeclaration(match);
+        }
+
+        public string Name
+        {
+            get { return match.Groups["name"].Value; }
+        }
+
+        public bool IsExplicitImplementation
+        {
+            get { return match.Groups["namedot"].Success; }
+        }
+    }
+}
